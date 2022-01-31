c316715c2ee9e3496c556ec03e360721832b7bc0
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-01-31 17:46:16 -0500

    cambiando tema en el select options

--- scripts/cambiarTema.js
+++ scripts/cambiarTema.js
@@ -3,6 +3,7 @@ let btnDark = document.querySelector('#dark');
 let nav = document.querySelector(".navbar-brand")
 let hr = document.getElementById('hr');
 let search = document.getElementById('search')
+let selectFil = document.getElementById('select');
 
   btnDark.addEventListener('click', () => {
     const body = document.querySelector("#body");
@@ -13,6 +14,8 @@ let search = document.getElementById('search')
     hr.style.backgroundColor ="#FFFF";
     search.style.backgroundColor = "hsl(240, 14%, 15%)";
     search.style.color = "#FFFF";
+    selectFil.style.backgroundColor = "hsl(240, 14%, 15%)";
+    selectFil.style.color = "#FFFF";
     
     
   })
