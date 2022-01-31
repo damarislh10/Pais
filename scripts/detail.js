6519b5bed9b58870559166cdcaa1de16ceb509f8
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-01-31 14:29:02 -0500

    agregando estilos detail

--- scripts/detail.js
+++ scripts/detail.js
@@ -7,15 +7,7 @@ const getDetalle = () => {
     const {name,population,region,capital,img,Currency,President} = detallePais;
 
     container.innerHTML += `
-    <div class="" style="width: 20rem;">
-    <img src="${img}" class="" card-bande" alt="...">
-    <div class="">
-    <h1 class = "fs-5"> ${name}</h1>
-    <p><span class="negrita">Population:</span> ${population}</p>
-    <p><span class="negrita">Region:</span> ${region} </p>
-    <p><span class="negrita">Capital:</span> ${capital}  </p>
-    </div>
-  </div>
+
     `
 }
 
