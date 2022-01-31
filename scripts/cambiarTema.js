f3ff15f088e9211d61ef647ad7eb378c6f85b017
Brahian Alzate <brahianalzate02@gmail.com>
2022-01-31 15:46:39 -0500

    cards dark modifail

--- scripts/cambiarTema.js
+++ scripts/cambiarTema.js
@@ -1,10 +1,14 @@
+
+
 let btnDark = document.querySelector('#dark');
 let nav = document.querySelector(".navbar-brand")
 let hr = document.getElementById('hr');
 let search = document.getElementById('search')
 
+
   btnDark.addEventListener('click', () => {
     const body = document.querySelector("#body");
+    let pintar = document.querySelector ('.pintar')
     body.style.backgroundColor = "hsl(240, 14%, 15%)";
     nav.style.color = "#FFFF"
     btnDark.style.color = "#FFFF";
@@ -12,7 +16,8 @@ let search = document.getElementById('search')
     hr.style.backgroundColor ="#FFFF";
     search.style.backgroundColor = "hsl(240, 14%, 15%)";
     search.style.color = "#FFFF";
-
+    pintar.style.backgroundColor = "hsl(240, 18%, 19%)";
+    pintar.style.color = "#FFFF";
   })
 
 
--- scripts/showPais.js
+++ scripts/showPais.js
@@ -1,14 +1,17 @@
+
 export const showPais = (datos, contenedor) => {
     datos.forEach(element => {
         const {name, id, population, region, capital, img} = element
         contenedor.innerHTML += `
-        <div class="card" style="width: 16rem;">
+        <div class="card pintar" style="width: 16rem;">
+    
         <img id="${id}" src="${img}" class="card-img-top card-bande" alt="...">
         <div class="card-body">
         <h1 class = "fs-5"> ${name}</h1>
         <p><span class="negrita">Population:</span> ${population}</p>
         <p><span class="negrita">Region:</span> ${region} </p>
         <p><span class="negrita">Capital:</span> ${capital}  </p>
+    
         </div>
       </div>
         `
