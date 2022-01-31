504f220c809e1bcd8983072a597acc27db8088ea
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-01-31 10:13:28 -0500

    cambiando componentes

--- scripts/cambiarTema.js
+++ scripts/cambiarTema.js
@@ -1,10 +1,14 @@
 let btnDark = document.querySelector('#dark');
 let nav = document.querySelector(".navbar-brand")
+let hr = document.getElementById('hr');
 
   btnDark.addEventListener('click', () => {
     const body = document.querySelector("#body");
     body.style.backgroundColor = "hsl(240, 14%, 15%)";
     nav.style.color = "#FFFF"
+    btnDark.style.color = "#FFFF";
+    btnDark.style.backgroundColor ="hsl(240, 14%, 15%)"
+    hr.style.backgroundColor ="#FFFF"
   })
 
 
