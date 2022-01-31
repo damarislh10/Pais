9ef98a0262f472e3de39766eeb9d8294b84af874
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-01-31 13:10:58 -0500

    id localStorage

--- scripts/showPais.js
+++ scripts/showPais.js
@@ -2,14 +2,8 @@ export const showPais = (datos, contenedor) => {
     datos.forEach(element => {
         const {name, id, population, region, capital, img} = element
         contenedor.innerHTML += `
-<<<<<<< HEAD
-        <div class="card" style="width: 18rem;">
-  
-        <img id=${id} src="${img}" class="card-img-top card-bande" alt="...">
-=======
         <div class="card" style="width: 16rem;">
-        <img src="${img}" class="card-img-top" alt="...">
->>>>>>> 158be70305d16510932cef0b9daee02cfe3aba3c
+        <img id="${id}" src="${img}" class="card-img-top card-bande" alt="...">
         <div class="card-body">
         <h1 class = "fs-5"> ${name}</h1>
         <p><span class="negrita">Population:</span> ${population}</p>
