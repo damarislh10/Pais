abb3e21946b6b767b694c087abda8e748701f9a4
damarislh10 <cortezhernandezdamaris@gmail.com>
2022-01-31 18:42:02 -0500

    filter en consola

--- scripts/filtrar.js
+++ scripts/filtrar.js
@@ -2,13 +2,30 @@ import { getPais } from "./getPais.js";
 import { urlPais } from "./url.js";
 import { showPais } from "./showPais.js";
 
-const filter = document.getElementById('select');
-// const filter = document.querySelectorAll('.dropdown-item')
-console.log(filter[0].value);
-filter.addEventListener('click' , async() => {
-    console.log(filter[0].value);
+// const filter = document.getElementById('select');
+// const filter = document.querySelector('#select').value;
+// console.log(options[0].value);
+// console.log(filter[0].value);
+
+
+let selectValue = document.getElementById('select');
+
+selectValue.addEventListener('change', async(e) =>{
     const data = await getPais();
-    showPais(data,contenedor)
-    console.log (data)
-    
+
+    let options = e.target.value;
+ 
+ 
+    data.forEach(element => {
+         const {id, name, img, population,region, capital} = element;
+
+        if(region === options ) {
+            console.log(region + options);
+        }      
+    })
+  
+    // const data = await getPais();
+    // showPais(data,contenedor)
+    // console.log (data)  
+
 })
